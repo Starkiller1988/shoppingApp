8db94689aa1c80887e6dbb7280a5248c67b48442
Samir Schabel <schabel.samir@gmail.com>
2021-10-08 17:02:34 +0200

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, {useEffect} from "react";
+import ReactDOM from 'react-dom'
+import './index.css'
+import shoppingList from '../src/'
 
-function App() {
-  const [count, setCount] = useState(0)
+const shoppingList = () => {
+  useEffect(() => {
+    const url="https://fetch-me.vercel.app/shopping-list.json";
 
-  return (
-    <div className="App">
-     
-    </div>
-  )
+const fetchData = async() => {
+try{
+  const responce = await fetch(url);
+  const json = await response.json();
+  console.log(json);
+} catch (error) {
+console.log("error", error);
 }
+};
 
-export default App
+fetchData();
+
+  },[]);
+
+  return <div></div>;
+};
+
+
+
+export default App;
